1f2a65b1709e98aa5b2b65cd294500a38d46c084
Vignesh Rajaram <vigneshwarank98@gmail.com>
2021-02-13 13:08:37 +0530

    Initial Commit

---
+++ script.py
@@ -0,0 +1,20 @@
+# import math
+# import sys
+# from os import rename
+
+name = input("Your Name: ")
+print("Hi!!", name)
+
+# import requests
+
+# r = requests.get("https://coreyms.com")
+
+
+# def greet(who_to_greet):
+#    greeting = f"Hello, {who_to_greet}"
+#    return greeting
+
+
+# print(r.status_code)
+# print(r.ok)
+# print(greet("Vicky"))
